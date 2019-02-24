2ec149461cd026941e743e02093a97666eda03ed
Arif Hasan <arif25169@gmail.com>
2019-02-24 17:36:36 +0600

    https

--- src/api/fetchLaptops.js
+++ src/api/fetchLaptops.js
@@ -10,12 +10,12 @@ export const fetchLaptops = async () => {
     //     resolve(laptops);
     // });
 
-    const { body } = await request.get('http://www.mocky.io/v2/5c7276743300007500760198');
+    const { body } = await request.get('https://www.mocky.io/v2/5c7276743300007500760198');
     return body.laptops;
 };
 
 export const fetchLaptopById = async id => {
-    const { body } = await request.get('http://www.mocky.io/v2/5c7276743300007500760198');
+    const { body } = await request.get('https://www.mocky.io/v2/5c7276743300007500760198');
     let laptops = body.laptops;
     return new Promise((resolve, reject) => {
         // console.log("Id in api fetchLaptops " , id);
@@ -28,6 +28,6 @@ export const fetchCategories = async () => {
     // return new Promise(resolve =>{
     //     resolve(mockCategories);
     // });
-    const { body } = await request.get('http://www.mocky.io/v2/5c726dac330000750076018c');
+    const { body } = await request.get('https://www.mocky.io/v2/5c726dac330000750076018c');
     return body.categories;
 }
